4247aa4d5c8d2441b36ddb9df22bed4e00bb6d52
GhostGrid-admin <ghostgrid.info@gmail.com>
2025-06-16 16:49:19 +0200

    Update firebase-init.js

--- scripts/firebase-init.js
+++ scripts/firebase-init.js
@@ -6,13 +6,12 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/11.8.1/firebase
 import { getStorage } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCjWQB0hAO1r686hVdpBKBJkaIYMXOM8l8",
-  authDomain: "ghostgrid-cebe9.firebaseapp.com",
-  projectId: "ghostgrid-cebe9",
-  storageBucket: "ghostgrid-cebe9.appspot.com",
-  messagingSenderId: "346259562017",
-  appId: "1:346259562017:web:cf0166021bb45d72768d60",
-  measurementId: "G-4BFC1G32H7"
+  apiKey: "AIzaSyDdMt1JyT-Mz2eOVeH0GvqJMijtIIZRORs",
+  authDomain: "ghostgrid-fb278.firebaseapp.com",
+  projectId: "ghostgrid-fb278",
+  storageBucket: "ghostgrid-fb278.appspot.com",
+  messagingSenderId: "283208202937",
+  appId: "1:283208202937:web:11c0cea81fd18a31163968"
 };
 
 const app  = initializeApp(firebaseConfig);
@@ -20,6 +19,7 @@ const auth = getAuth(app);
 const db   = getFirestore(app);
 const storage = getStorage(app);
 
+// Detta är bara för debug/felsökning:
 onAuthStateChanged(auth, user => {
   console.log(user ? `Inloggad som ${user.email}` : "Ingen användare inloggad");
 });
