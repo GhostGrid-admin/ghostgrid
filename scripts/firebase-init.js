8f5a4fc3c2f0e383dd893df8e3bbc3fe1b0d7a73
GhostGrid-admin <ghostgrid.info@gmail.com>
2025-06-16 11:34:34 +0200

    Update firebase-init.js

--- scripts/firebase-init.js
+++ scripts/firebase-init.js
@@ -1,11 +1,10 @@
 // scripts/firebase-init.js
 
-// 1. Importera bara modular‑API från CDN
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-storage.js";
 
-// 2. Din Firebase‑konfiguration (uppdatera om du bytt projekt)
 const firebaseConfig = {
   apiKey: "AIzaSyDUem8OciTynoSujYyZSQqsExocoZeJu0Y",
   authDomain: "ghostgrid-cebe9.firebaseapp.com",
@@ -16,15 +15,13 @@ const firebaseConfig = {
   measurementId: "G-4BFC1G32H7"
 };
 
-// 3. Initiera
 const app  = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db   = getFirestore(app);
+const storage = getStorage(app);
 
-// 4. (Valfritt) visa inloggningsstatus i konsolen
 onAuthStateChanged(auth, user => {
   console.log(user ? `Inloggad som ${user.email}` : "Ingen användare inloggad");
 });
 
-// 5. Exportera för andra moduler
-export { auth, db };
+export { auth, db, storage };
