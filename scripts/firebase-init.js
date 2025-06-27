364b7ab984cdb5706a06f8b77449ae217ffd685d
GhostGrid-admin <ghostgrid.info@gmail.com>
2025-06-27 03:51:09 +0200

    Update firebase-init.js

--- scripts/firebase-init.js
+++ scripts/firebase-init.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCjWQB0hAO1r686hVdpBKBJkaIYMXOM8l8",
   authDomain: "ghostgrid-fb278.firebaseapp.com",
   projectId: "ghostgrid-fb278",
-  storageBucket: "ghostgrid-fb278.firebasestorage.app",
+  storageBucket: "ghostgrid-fb278.appspot.com",
   messagingSenderId: "283208202937",
   appId: "1:283208202937:web:11c0cea81fd18a31163968"
 };
