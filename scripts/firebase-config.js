64cc2d670b9f0f908e0f435ac329a36067fe180d
GhostGrid-admin <ghostgrid.info@gmail.com>
2025-07-11 02:59:56 +0200

    Create firebase-config.js

---
+++ scripts/firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js (används BARA för vanliga script-taggar, INTE för import/export)
+const firebaseConfig = {
+  apiKey: "AIzaSyCjWQB0hAO1r686hVdpBKBJkaIYMXOM8l8",
+  authDomain: "ghostgrid-fb278.firebaseapp.com",
+  projectId: "ghostgrid-fb278",
+  storageBucket: "ghostgrid-fb278.appspot.com",
+  messagingSenderId: "283208202937",
+  appId: "1:283208202937:web:11c0cea81fd18a31163968"
+};
+firebase.initializeApp(firebaseConfig);
+// Inget mer!
