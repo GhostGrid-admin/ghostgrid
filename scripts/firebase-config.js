6bb172066ab36fb7db133c8114a012c912fbaaac
GhostGrid-admin <ghostgrid.info@gmail.com>
2025-07-11 03:32:30 +0200

    Update firebase-config.js

--- scripts/firebase-config.js
+++ scripts/firebase-config.js
@@ -1,11 +1,15 @@
-// firebase-config.js (används BARA för vanliga script-taggar, INTE för import/export)
-const firebaseConfig = {
-  apiKey: "AIzaSyCjWQB0hAO1r686hVdpBKBJkaIYMXOM8l8",
-  authDomain: "ghostgrid-fb278.firebaseapp.com",
-  projectId: "ghostgrid-fb278",
-  storageBucket: "ghostgrid-fb278.appspot.com",
-  messagingSenderId: "283208202937",
-  appId: "1:283208202937:web:11c0cea81fd18a31163968"
-};
-firebase.initializeApp(firebaseConfig);
-// Inget mer!
+// firebase-config.js
+if (!window.firebaseAppInitialized) {
+  const firebaseConfig = {
+    apiKey: "AIzaSyCjWQB0hAO1r686hVdpBKBJkaIYMXOM8l8",
+    authDomain: "ghostgrid-fb278.firebaseapp.com",
+    projectId: "ghostgrid-fb278",
+    storageBucket: "ghostgrid-fb278.appspot.com",
+    messagingSenderId: "283208202937",
+    appId: "1:283208202937:web:11c0cea81fd18a31163968"
+  };
+  firebase.initializeApp(firebaseConfig);
+  window.db = firebase.firestore();
+  window.auth = firebase.auth();
+  window.firebaseAppInitialized = true;
+}
